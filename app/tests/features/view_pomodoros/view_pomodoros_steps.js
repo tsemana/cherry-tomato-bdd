0b3660795a4562d277d000cd7be42ccea2c13ded
Tony Semana <tony.semana@semanteks.com>
2016-07-04 02:49:31 -0400

    Do the simplest thing that will make the tests pass
    WHAT
    - Skip the authentication step; assumes always logged in for now
    - Skip the second given; no pomodoros to worry about yet
    - PomodoroManager.myCompletedPomodoros is a
      programming-by-wishful-thinking construct
    - Code everything in the test steps file
    
    WHY
    - "Simplest thing that will pass" is a basic principle of TDD/BDD. It:
      - Allows you to move quickly from red to green
      - Forces you to take small steps
      - Allows you to defer thinking about 'other scenarios'
    - The premise is, the code will be improved as:
      - More scenarios are added and so the code must evolve to address
        these
      - Code is refactored with each evolution/iteration
    - As of now, this scenario passes, and nothing is forcing a refactor of
      the PomodoroManager to it's own module

--- app/tests/features/view_pomodoros/view_pomodoros_steps.js
+++ app/tests/features/view_pomodoros/view_pomodoros_steps.js
@@ -3,22 +3,30 @@
 
 module.exports = function steps() {
   this.Given(/^I am authenticated$/, () => {
-    // Write the automation code here
-    pending();
+    // Skip this detail for for; assumaes always authenticated
   });
 
   this.Given(/^I have no pomodoros$/, () => {
-    // Write the automation code here
-    pending();
+    // No pomodoros, check! :)
   });
 
+  const PomodoroManager = {
+    myCompletedPomodoros: () => 'No Pomodoros yet',
+  };
+
+  let myPomodoros;
+
   this.When(/^I review my activity$/, () => {
-    // Write the automation code here
-    pending();
+    // Naive implementation of 'BDD' is to assume this
+    // step should be based on UI. The expected step is to
+    // start looking for a screen with a list or an
+    // empty state message..
+    // We're going to start with an API call through a
+    // kind of programming by wishfull thinking
+    myPomodoros = PomodoroManager.myCompletedPomodoros();
   });
 
   this.Then(/^I should be informed that I have none$/, () => {
-    // Write the automation code here
-    pending();
+    expect(myPomodoros).toEqual('No Pomodoros yet');
   });
 };
