7500eef435f0b64e3136dbe7c4e588e5d6d145c5
Tony Semana <tony.semana@semanteks.com>
2016-07-04 01:16:55 -0400

    Add Cucumber smoke test
    - Add feature smoke test to shake down chimpjs/cucumber setup
    - Set expect to fail intentionally

---
+++ app/tests/features/smoke.feature
@@ -0,0 +1,9 @@
+Feature: Smoke Test
+
+  As a dev I want to make sure my testing
+  framework is up
+
+  @watch
+  Scenario: Find home
+    Given I am home
+    Then I should see "Welcome to Meteor!"
---
+++ app/tests/features/smoke_steps.js
@@ -0,0 +1,15 @@
+/* eslint new-cap: 0 */
+/* global browser, expect, pending, Meteor */
+
+module.exports = function steps() {
+  this.Given(/^I am home$/, () => {
+    browser.url('http:localhost:3000');
+  });
+
+  this.Then(/^I should see "([^"]*)"$/, (welcomeMessage) => {
+    const el = browser.element('h1');
+    el.waitForExist();
+    expect(el.getText()).toEqual('This should fail');
+  });
+};
+
