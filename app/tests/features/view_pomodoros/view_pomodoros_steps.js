6d26b2af5157996b41262cab52a5108444c33795
Tony Semana <tony.semana@semanteks.com>
2016-07-04 02:46:06 -0400

    Add pending step boilerplate to steps file
    WHAT
    - Add all steps to one file
    
    WHY
    - Chimp boilerplate recommends one step per file, but personal
      preference is to keep the feature steps together at least at first

---
+++ app/tests/features/view_pomodoros/view_pomodoros_steps.js
@@ -0,0 +1,24 @@
+/* eslint new-cap: 0 */
+/* global browser, expect, pending, Meteor */
+
+module.exports = function steps() {
+  this.Given(/^I am authenticated$/, () => {
+    // Write the automation code here
+    pending();
+  });
+
+  this.Given(/^I have no pomodoros$/, () => {
+    // Write the automation code here
+    pending();
+  });
+
+  this.When(/^I review my activity$/, () => {
+    // Write the automation code here
+    pending();
+  });
+
+  this.Then(/^I should be informed that I have none$/, () => {
+    // Write the automation code here
+    pending();
+  });
+};
